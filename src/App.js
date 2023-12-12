6b054234ecf292f8b93be7cedcc207aed1368195
developer-belal <theotherface.bk@gmail.com>
2023-12-12 17:52:41 +0300

    nn

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
     <h2>no body</h2>
     <h1 className="title">hello word</h1>
     <h2>no body</h2>
+    <h1 className="title">hello word</h1>
+    <h2>no body</h2>
+    <h1 className="title">hello word</h1>
+    <h2>no body</h2>
 </>
     
   );
