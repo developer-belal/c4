6330f63e3e7d5478b73c925def5b045eb3b61b0b
developer-belal <theotherface.bk@gmail.com>
2023-12-12 17:47:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+  return (
+    
+<>
+    <h1 className="title">hello word</h1>
+    <h2>no body</h2>
+    <h1 className="title">hello word</h1>
+    <h2>no body</h2>
+</>
+    
+  );
+}
+
+export default App;
